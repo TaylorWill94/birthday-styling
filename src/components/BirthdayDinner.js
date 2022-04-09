460cd371876838cb8eb55dfae5ac2dab46112880
Taylor W <taylorwilliams@pursuit.org>
2022-04-09 15:06:13 -0400

    Added outfits

--- src/components/BirthdayDinner.js
+++ src/components/BirthdayDinner.js
@@ -1,5 +1,8 @@
 import gucci from "../images/gucci.jpg";
 import area from "../images/area.jpg";
+import dolce from "../images/dolce.jpg";
+import ysl from "../images/ysl-heels.jpg";
+import mesh from "../images/mesh-dress.jpg";
 
 function BirthdayDinner() {
   return (
@@ -8,36 +11,87 @@ function BirthdayDinner() {
       <section className="outfit-sec">
         <br />
         <br />
-        <a
-          href="https://www.gucci.com/us/en/pr/women/ready-to-wear-for-women/dresses-for-women/jumpsuits-for-women/faille-jumpsuit-with-g-buckle-belt-p-674467Z8AUI1000"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img className="img" src={gucci} alt="gucci-jumpsuit" width="500" />
-        </a>
+        <div className="column">
+          <p>Outfit One</p>
+          <br />
+          <a
+            href="https://www.gucci.com/us/en/pr/women/ready-to-wear-for-women/dresses-for-women/jumpsuits-for-women/faille-jumpsuit-with-g-buckle-belt-p-674467Z8AUI1000"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className="img" src={gucci} alt="gucci-jumpsuit" width="350" />
+          </a>
+
+          <br />
+          <br />
+          <a
+            href="https://www.gucci.com/us/en/pr/women/ready-to-wear-for-women/dresses-for-women/jumpsuits-for-women/faille-jumpsuit-with-g-buckle-belt-p-674467Z8AUI1000"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="gucci-btn">GUCCI JUMPSUIT</button>
+          </a>
+          <img src={dolce} alt="dolce" width="300" />
+          <a
+            href="https://www.saksfifthavenue.com/product/dolce-gabbana-sculpted-heel-metallic-leather-sandals-0400013535394.html?dwvar_0400013535394_color=GOLD"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="gucci-btn">DOLCE HEELS</button>
+          </a>
+        </div>
         <br />
         <br />
-        <a
-          href="https://www.gucci.com/us/en/pr/women/ready-to-wear-for-women/dresses-for-women/jumpsuits-for-women/faille-jumpsuit-with-g-buckle-belt-p-674467Z8AUI1000"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button className="gucci-btn">GUCCI JUMPSUIT</button>
-        </a>
+        <div className="column">
+          <p>Outfit Two</p>
+          <br />
+          <a href="https://www.farfetch.com/shopping/women/area-crystal-embellished-mini-dress-item-17266847.aspx?storeid=13537">
+            <img className="img" src={area} alt="area-dress" width="250" />
+          </a>
+          <br />
+          <br />
+          <a
+            href="https://www.farfetch.com/shopping/women/area-crystal-embellished-mini-dress-item-17266847.aspx?storeid=13537"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="gucci-btn">AREA CRYSTAL DRESS</button>
+          </a>
+          <img src={ysl} alt="ysl" width="300" />
+          <a
+            href="https://www.saksfifthavenue.com/product/saint-laurent-opyum-leather-sandals-0400099522701.html?dwvar_0400099522701_color=BLACK"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="gucci-btn">YSL HEELS</button>
+          </a>
+        </div>
         <br />
         <br />
-        <a href="https://www.farfetch.com/shopping/women/area-crystal-embellished-mini-dress-item-17266847.aspx?storeid=13537">
-          <img className="img" src={area} alt="area-dress" width="400" />
-        </a>
-        <br />
-        <br />
-        <a
-          href="https://www.farfetch.com/shopping/women/area-crystal-embellished-mini-dress-item-17266847.aspx?storeid=13537"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button className="gucci-btn">AREA CRYSTAL DRESS</button>
-        </a>
+        <div className="column">
+          <p>Outfit Two</p>
+          <br />
+          <a href="https://www.ssense.com/en-us/women/product/gauntlett-cheng/brown-polyester-mini-dress/9825471">
+            <img className="img" src={mesh} alt="gauntlett-cheng" width="250" />
+          </a>
+          <br />
+          <br />
+          <a
+            href="https://www.farfetch.com/shopping/women/area-crystal-embellished-mini-dress-item-17266847.aspx?storeid=13537"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="gucci-btn">AREA CRYSTAL DRESS</button>
+          </a>
+          <img src={ysl} alt="ysl" width="300" />
+          <a
+            href="https://www.saksfifthavenue.com/product/saint-laurent-opyum-leather-sandals-0400099522701.html?dwvar_0400099522701_color=BLACK"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="gucci-btn">YSL HEELS</button>
+          </a>
+        </div>
       </section>
     </div>
   );
--- src/components/BirthdayOutfits.js
+++ src/components/BirthdayOutfits.js
@@ -1,5 +1,45 @@
+import gucci from "../images/gucci-top.jpg";
+import wang from "../images/alexander-wang.jpg";
+
 function BirthdayOutfits() {
-  return <div className="outfits">Birthday Outfits</div>;
+  return (
+    <div className="outfits">
+      Birthday Outfits
+      <section className="outfit-sec">
+        <br />
+        <br />
+        <div className="column">
+          <p>Outfit One</p>
+          <br />
+          <a
+            href="https://www.gucci.com/us/en/pr/women/ready-to-wear-for-women/tops-shirts-for-women/tops-for-women/gg-jersey-jacquard-sleeveless-top-p-693286XJEFY7388"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className="img" src={gucci} alt="gucci-top" width="350" />
+          </a>
+
+          <br />
+          <br />
+          <a
+            href="https://www.gucci.com/us/en/pr/women/ready-to-wear-for-women/tops-shirts-for-women/tops-for-women/gg-jersey-jacquard-sleeveless-top-p-693286XJEFY7388"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="gucci-btn">GUCCI TOP</button>
+          </a>
+          <img src={wang} alt="alexander-wang" width="300" />
+          <a
+            href="https://www.bloomingdales.com/shop/product/alexander-wang-womens-julie-strappy-high-heel-sandals?ID=3924304&pla_country=US&cm_mmc=Google-PLA-ADC-_-GMM2+-+Center+Core+-+Shoes-_-Shoes-_-192722200575USA-_-go_cmp-13048290360_adg-123409840018_ad-520724334467_pla-1246168337695_dev-c_ext-_prd-192722200575USA&gclid=CjwKCAjw3cSSBhBGEiwAVII0Z7CXcfZSLtpRPLHr1eB75Co5TYQGZSeJcVsJjk60NVzzsh9WlWXBfRoC2cQQAvD_BwE"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="gucci-btn">ALEXANDER WANG HEELS</button>
+          </a>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default BirthdayOutfits;
